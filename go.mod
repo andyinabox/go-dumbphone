df48fff0190d244dc9b294100348c460753b4aa1
Andy Dayton <amdayton@gmail.com>
2019-10-16 15:16:30 -0500

    Start build directions module

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/andyinabox/go-dumbphone
 
 go 1.13
 
-require github.com/sakirsensoy/genv v1.0.1
+require (
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/sakirsensoy/genv v1.0.1
+	github.com/sergi/go-diff v1.0.0 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/time v0.0.0-20190921001708-c4c64cad1fd0 // indirect
+	googlemaps.github.io/maps v0.0.0-20191014172202-ce2e58e026c5
+)
--- pkg/directions/directions.go
+++ pkg/directions/directions.go
@@ -2,6 +2,47 @@
 
 package directions
 
-func Directions() {
+import (
+	"context"
+	"log"
+	"strconv"
+	"text/template"
+	"time"
 
+	"googlemaps.github.io/maps"
+)
+
+const (
+	travelMode = "driving"
+	imgTpl     = "https://maps.googleapis.com/maps/api/staticmap?size=%dx%d&path=enc:%s&key="
+	imgW, imgH = 230, 230
+)
+
+// GoogleMapsData data for google maps directions
+type GoogleMapsData struct {
+	APIKey      string
+	Origin      string
+	Destination string
+	Mode        string
+	Time        string
+}
+
+// Directions get directions for a set of directions data
+func Directions(data GoogleMapsData) []maps.Route {
+	c, err := maps.NewClient(maps.WithAPIKey(data.APIKey))
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+	r := &maps.DirectionsRequest{
+		Origin:        data.Origin,
+		Destination:   data.Destination,
+		Mode:          maps.Mode(data.Mode),
+		DepartureTime: data.Time,
+	}
+	route, _, err := c.Directions(context.Background(), r)
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
+
+	return route
 }
