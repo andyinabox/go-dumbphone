50a3b6a271f780db3373260ccd56366d5fcaad10
Andy Dayton <amdayton@gmail.com>
2019-10-18 15:34:24 -0500

    Update app info and add version

--- cmd/dumbp/main.go
+++ cmd/dumbp/main.go
@@ -8,14 +8,18 @@ import (
 	"os"
 )
 
-func main() {
-	app := cli.NewApp()
+const version = "0.1.0"
 
+func main() {
 	err := godotenv.Load()
 	if err != nil {
 		panic("Error loading .env file")
 	}
 
+	app := cli.NewApp()
+	app.Name = "dumbphone"
+	app.Usage = "A set of tools to make your dumbphone a little smarter"
+	app.Version = version
 	app.Commands = []cli.Command{
 		// directions
 		commands.DirectionsSubcommand,
